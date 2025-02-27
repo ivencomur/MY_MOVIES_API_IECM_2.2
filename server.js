5f7bd792c187fc827cbd03c09da25cdc3b96f3d2
ivencomur <ivancortes@hotmail.com>
2025-02-27 18:33:54 -0500

    CORRECTIONS
    Corrected per feedback received

--- server.js
+++ server.js
@@ -1,17 +1,54 @@
-const http = require('http');
-const fs = require('fs');
-
-function log(message) {
-  const timestamp = new Date().toISOString();
-  fs.appendFileSync('log.txt', `[${timestamp}] ${message}\n`);
-  console.log(message); 
-}
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
 
 http.createServer((request, response) => {
-  log(`GET ${request.url} HTTP/1.1 from ${request.socket.remoteAddress}`);
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello Node!\n');
-  log(`200 OK - Content Length: 12`);
+  let addr = request.url,
+    q = new URL(addr, 'http://' + request.headers.host),
+    filePath = '';
+
+  const logMessage = 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n';
+
+  fs.appendFile('log.txt', logMessage, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+    console.log(logMessage);
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = __dirname + '/documentation.html';
+  } else if (q.pathname.includes('css')) {
+    filePath = __dirname + q.pathname;
+  } else if (q.pathname.includes('img')) {
+      filePath = __dirname + q.pathname;
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      response.writeHead(404);
+      response.end('File not found');
+      console.log(err);
+      return;
+    }
+
+    let contentType = 'text/html';
+    if (filePath.endsWith('.css')) {
+      contentType = 'text/css';
+    } else if (filePath.endsWith('.svg')) {
+      contentType = 'image/svg+xml';
+    } else if (filePath.endsWith('.png')) {
+      contentType = 'image/png';
+    }
+
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.write(data);
+    response.end();
+  });
 }).listen(8080);
 
-log('Server started on port 8080');
+console.log('My test server is running on Port 8080.');
